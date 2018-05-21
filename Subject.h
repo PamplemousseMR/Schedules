01f13702010731e98e71b7763d45584f6a528d8e
pamplemousse26 <romain.manciaux@gmail.com>
2018-05-21 23:50:36 +0200

    feat(schedules): get random subject

--- Subject.h
+++ Subject.h
@@ -18,6 +18,22 @@ public:
 	
 	static const std::vector<Subject> s_subjects;
 
+	static unsigned short s_s1Count;
+
+	static unsigned short s_s1Offset;
+
+	static unsigned short s_s2Count;
+
+	static unsigned short s_s2Offset;
+
+	static unsigned short s_s3Count;
+
+	static unsigned short s_s3Offset;
+
+	static unsigned short s_s4Count;
+
+	static unsigned short s_s4Offset;
+
 public:
 	
 	enum SUBJECT_SPECIALITY
@@ -37,6 +53,27 @@ public:
 		S4
 	};
 
+	enum SUBJECT_TYPE
+	{
+		MATHS,
+		PHYSICS,
+		COMPUTER_SCIENCE,
+		CHEMISTRY,
+		ENGLISH,
+		FRENCH,
+		UNDEFINED
+	};
+
+	static const unsigned short SUBJECT_MODALITY_SIZE;
+
+	enum SUBJECT_MODALITY
+	{
+		CM,
+		CI,
+		TD,
+		TP
+	};
+
 public:
 	
 	const std::string m_name;
@@ -45,13 +82,9 @@ public:
 
 	const SUBJECT_SPECIALITY m_speciality;
 
-	const unsigned short m_nbHoursCM;
+	const SUBJECT_TYPE m_type;
 
-	const unsigned short m_nbHoursCI;
-
-	const unsigned short m_nbHoursTD;
-
-	const unsigned short m_nbHoursTP;
+	std::map<SUBJECT_MODALITY, unsigned short> m_modality;
 
 public:
 
@@ -59,7 +92,7 @@ public:
 
 private:
 
-	Subject(const std::string&, SUBJECT_SEMESTER, SUBJECT_SPECIALITY, unsigned short, unsigned short, unsigned short, unsigned short);
+	Subject(const std::string&, SUBJECT_SEMESTER, SUBJECT_SPECIALITY, SUBJECT_TYPE, unsigned short, unsigned short, unsigned short, unsigned short);
 
 };
 
@@ -69,73 +102,92 @@ private:
 
 const std::vector<Subject> Subject::s_subjects
 {
-	Subject("UE101", S1, COMMUN, 0, 42, 0, 0),
-	Subject("UE102", S1, COMMUN, 0, 42, 0, 0),
-	Subject("UE103", S1, COMMUN, 0, 42, 0, 0),
-	Subject("UE104", S1, COMMUN, 0, 56, 0, 0),
-	Subject("UE105", S1, COMMUN, 0, 42, 0, 42),
-	Subject("UE106", S1, COMMUN, 0, 70, 0, 42),
-	Subject("UE107", S1, COMMUN, 0, 0, 0, 14),
+	Subject("UE101", S1, COMMUN, MATHS, 0, 42, 0, 0),
+	Subject("UE102", S1, COMMUN, PHYSICS, 0, 42, 0, 0),
+	Subject("UE103", S1, COMMUN, CHEMISTRY, 0, 42, 0, 0),
+	Subject("UE104", S1, COMMUN, COMPUTER_SCIENCE, 0, 56, 0, 0),
+	Subject("UE105", S1, COMMUN, FRENCH, 0, 42, 0, 42),
+	Subject("UE106", S1, COMMUN, ENGLISH, 0, 70, 0, 42),
+	Subject("UE107", S1, COMMUN, UNDEFINED, 0, 0, 0, 14),
 
-	Subject("UE201_An", S2, COMMUN, 10, 32, 0, 0),
-	Subject("UE201_Al", S2, COMMUN, 10, 32, 0, 0),
-	Subject("UE202", S2, COMMUN, 14, 28, 0, 21),
-	Subject("UE203", S2, COMMUN, 10, 32, 0, 21),
-	Subject("UE204", S2, COMMUN, 14, 28, 0, 21),
-	Subject("UE205", S2, COMMUN, 0, 42, 0, 0),
-	Subject("UE206", S2, COMMUN, 0, 42, 0, 0),
-	Subject("UE207", S2, COMMUN, 0, 0, 8, 28),
+	Subject("UE201_An", S2, COMMUN, MATHS, 10, 32, 0, 0),
+	Subject("UE201_Al", S2, COMMUN, MATHS, 10, 32, 0, 0),
+	Subject("UE202", S2, COMMUN, PHYSICS, 14, 28, 0, 21),
+	Subject("UE203", S2, COMMUN, COMPUTER_SCIENCE, 10, 32, 0, 21),
+	Subject("UE204", S2, COMMUN, CHEMISTRY, 14, 28, 0, 21),
+	Subject("UE205", S2, COMMUN, FRENCH, 0, 42, 0, 0),
+	Subject("UE206", S2, COMMUN, ENGLISH, 0, 42, 0, 0),
+	Subject("UE207", S2, COMMUN, UNDEFINED, 0, 0, 8, 28),
 
-	Subject("UE301", S3, COMMUN, 30, 0, 42, 0),
-	Subject("UE302_M", S3, COMMUN, 21, 0, 21, 0),
-	Subject("UE302_T", S3, COMMUN, 18, 0, 18, 0),
-	Subject("UE302_F", S3, COMMUN, 9, 0, 9, 0),
-	Subject("UE303", S3, COMMUN, 12, 0, 12, 12),
-	Subject("UE304_F", S3, COMMUN, 0, 36, 0, 0),
-	Subject("UE304_E", S3, COMMUN, 0, 36, 0, 0),
+	Subject("UE301", S3, COMMUN, MATHS, 30, 0, 42, 0),
+	Subject("UE302_M", S3, COMMUN, PHYSICS, 21, 0, 21, 0),
+	Subject("UE302_T", S3, COMMUN, PHYSICS, 18, 0, 18, 0),
+	Subject("UE302_F", S3, COMMUN, PHYSICS, 9, 0, 9, 0),
+	Subject("UE303", S3, COMMUN, COMPUTER_SCIENCE, 12, 0, 12, 12),
+	Subject("UE304_F", S3, COMMUN, FRENCH, 0, 36, 0, 0),
+	Subject("UE304_E", S3, COMMUN, ENGLISH, 0, 36, 0, 0),
 
-	Subject("UE305a", S3, CHEMICAL, 30, 0, 21, 21),
-	Subject("UE306", S3, CHEMICAL, 15, 0, 15, 15),
+	Subject("UE305a", S3, CHEMICAL, CHEMISTRY, 30, 0, 21, 21),
+	Subject("UE306", S3, CHEMICAL, CHEMISTRY, 15, 0, 15, 15),
 
-	Subject("UE307", S3, COMPUTER, 21, 0, 21, 12),
-	Subject("UE308", S3, COMPUTER, 15, 0, 0, 21),
+	Subject("UE307", S3, COMPUTER, COMPUTER_SCIENCE, 21, 0, 21, 12),
+	Subject("UE308", S3, COMPUTER, COMPUTER_SCIENCE, 15, 0, 0, 21),
 
-	Subject("UE305b", S3, GEOPHYSICAL, 30, 0, 21, 21),
-	Subject("UE309", S3, GEOPHYSICAL, 0, 30, 0, 0),
+	Subject("UE305b", S3, GEOPHYSICAL, PHYSICS, 30, 0, 21, 21),
+	Subject("UE309", S3, GEOPHYSICAL, PHYSICS, 0, 30, 0, 0),
 
-	Subject("UE305c", S3, OILNGAS, 30, 0, 21, 21),
-	Subject("UE310", S3, OILNGAS, 15, 0, 9, 6),
+	Subject("UE305c", S3, OILNGAS, PHYSICS, 30, 0, 21, 21),
+	Subject("UE310", S3, OILNGAS, PHYSICS, 15, 0, 9, 6),
 
-	Subject("UE401", S4, COMMUN, 30, 0, 42, 0),
-	Subject("UE402", S4, COMMUN, 36, 0, 36, 0),
-	Subject("UE403", S4, COMMUN, 12, 0, 12, 12),
-	Subject("UE404_F", S4, COMMUN, 0, 36, 0, 36),
-	Subject("UE404_E", S4, COMMUN, 0, 36, 0, 36),
-	Subject("UE405", S4, COMMUN, 6, 6, 0, 6),
+	Subject("UE401", S4, COMMUN, MATHS, 30, 0, 42, 0),
+	Subject("UE402", S4, COMMUN, PHYSICS, 36, 0, 36, 0),
+	Subject("UE403", S4, COMMUN, COMPUTER_SCIENCE, 12, 0, 12, 12),
+	Subject("UE404_F", S4, COMMUN, FRENCH, 0, 36, 0, 36),
+	Subject("UE404_E", S4, COMMUN, ENGLISH, 0, 36, 0, 36),
+	Subject("UE405", S4, COMMUN, UNDEFINED,6, 6, 0, 6),
 
-	Subject("UE406a", S4, CHEMICAL, 30, 0, 30, 21),
-	Subject("UE407", S4, CHEMICAL, 15, 0, 15, 15),
+	Subject("UE406a", S4, CHEMICAL, CHEMISTRY, 30, 0, 30, 21),
+	Subject("UE407", S4, CHEMICAL, CHEMISTRY, 15, 0, 15, 15),
 
-	Subject("UE408", S4, COMPUTER, 21, 0, 21, 12),
-	Subject("UE409", S4, COMPUTER, 18, 0, 18, 12),
+	Subject("UE408", S4, COMPUTER, COMPUTER_SCIENCE, 21, 0, 21, 12),
+	Subject("UE409", S4, COMPUTER, COMPUTER_SCIENCE, 18, 0, 18, 12),
 
-	Subject("UE4062", S4, GEOPHYSICAL, 30, 0, 30, 21),
-	Subject("UE410", S4, GEOPHYSICAL, 0, 30, 0, 0),
+	Subject("UE4062", S4, GEOPHYSICAL, PHYSICS, 30, 0, 30, 21),
+	Subject("UE410", S4, GEOPHYSICAL, PHYSICS, 0, 30, 0, 0),
 
-	Subject("UE406c", S4, OILNGAS, 30, 0, 30, 21),
-	Subject("UE411", S4, OILNGAS, 15, 0, 6, 9)
+	Subject("UE406c", S4, OILNGAS, PHYSICS, 30, 0, 30, 21),
+	Subject("UE411", S4, OILNGAS, PHYSICS, 15, 0, 6, 9)
 };
 
-Subject::Subject(const std::string& _name, SUBJECT_SEMESTER _semester, SUBJECT_SPECIALITY _speciality, unsigned short _cm,
-				 unsigned short _ci, unsigned short _td, unsigned short _tp)
+const unsigned short Subject::SUBJECT_MODALITY_SIZE = 4;
+
+unsigned short Subject::s_s1Count = 7;
+
+unsigned short Subject::s_s1Offset = 0;
+
+unsigned short Subject::s_s2Count = 8;
+
+unsigned short Subject::s_s2Offset = s_s1Count;
+
+unsigned short Subject::s_s3Count = 15;
+
+unsigned short Subject::s_s3Offset = s_s2Offset+s_s2Count;
+
+unsigned short Subject::s_s4Count = 14;
+
+unsigned short Subject::s_s4Offset = s_s3Offset+s_s3Count;
+
+Subject::Subject(const std::string& _name, SUBJECT_SEMESTER _semester, SUBJECT_SPECIALITY _speciality, SUBJECT_TYPE _type, 
+		unsigned short _cm, unsigned short _ci, unsigned short _td, unsigned short _tp)
 	:	m_name(_name),
 		m_semester(_semester),
 		m_speciality(_speciality),
-		m_nbHoursCM(_cm),
-		m_nbHoursCI(_ci),
-		m_nbHoursTD(_td),
-		m_nbHoursTP(_tp)
+		m_type(_type)
 {
+	m_modality[CM] = _cm;
+	m_modality[CI] = _ci;
+	m_modality[TD] = _td;
+	m_modality[TP] = _tp;
 }
  
 void Subject::display() const
@@ -160,8 +212,8 @@ void Subject::display() const
 	}
 	
 	std::cout << "Subject {" << m_name << "  " << semester << " " << speciality << "} =>" ;
-	std::cout << "\n\tCM : " << m_nbHoursCM << "H";
-	std::cout << "\n\tCI : " << m_nbHoursCI << "H";
-	std::cout << "\n\tTD : " << m_nbHoursTD << "H";
-	std::cout << "\n\tTP : " << m_nbHoursTP << "H" << std::endl;
+	std::cout << "\n\tCM : " << m_modality.at(CM) << "H";
+	std::cout << "\n\tCI : " << m_modality.at(CI) << "H";
+	std::cout << "\n\tTD : " << m_modality.at(TD) << "H";
+	std::cout << "\n\tTP : " << m_modality.at(TP) << "H" << std::endl;
 } 
